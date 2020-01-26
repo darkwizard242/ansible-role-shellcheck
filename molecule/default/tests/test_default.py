b3e7385f1c75feed22905a5613f237ed3d5ddf8c
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:30:51 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,23 +6,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-SHELLCHECK_BINARY_PATH = '/usr/bin/shellcheck'
-SHELLCHECK_DEBIAN_PACKAGE = 'shellcheck'
-SHELLCHECK_EL_PACKAGE = 'ShellCheck'
-
-
 def test_shellcheck_package_installed(host):
-    host.package("SHELLCHECK_DEBIAN_PACKAGE").is_installed or \
-      host.package("SHELLCHECK_EL_PACKAGE").is_installed
+    assert host.package("shellcheck").is_installed or \
+      host.package("ShellCheck").is_installed
 
 
 def test_shellcheck_binary_exists(host):
-    host.file('SHELLCHECK_BINARY_PATH').exists
+    assert host.file('/usr/bin/shellcheck').exists
 
 
 def test_shellcheck_binary_file(host):
-    host.file('SHELLCHECK_DEBIAN_BINARY_PATH').is_file
+    assert host.file('/usr/bin/shellcheck').is_file
 
 
-def test_shellcheck_binary_whereis(host):
-    host.check_output('whereis shellcheck') == SHELLCHECK_BINARY_PATH
+def test_shellcheck_binary_which(host):
+    assert host.check_output('which shellcheck') == '/usr/bin/shellcheck'
